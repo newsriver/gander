611159de5ea3b59f4dfd50fec6aa229816dacd3e
Elia Palme <elia.palme@newscron.com>
2016-09-08 08:34:39 +0200

    added structured text extraction
    In some scenarios it is useful to keep a minimum structure of the text
    (e.g. readability features). I have added an additional filed called
    structuredText that will contain the primary text including some white
    listed html tags.

--- src/main/scala/com/intenthq/gander/Gander.scala
+++ src/main/scala/com/intenthq/gander/Gander.scala
@@ -5,6 +5,7 @@ import java.util.Date
 import com.intenthq.gander.extractors.ContentExtractor._
 import com.intenthq.gander.opengraph.OpenGraphData
 import org.jsoup.Jsoup
+import org.jsoup.safety.Whitelist
 
 import scala.util.Try
 
@@ -19,10 +20,15 @@ case class PageInfo(title: String,
                     canonicalLink: Option[String],
                     openGraphData: OpenGraphData,
                     cleanedText: Option[String] = None,
+                    structuredText: Option[String] = None,
                     links: Seq[Link] = Seq.empty,
                     publishDate: Option[Date] = None)
 
 object Gander {
+
+  val whiteList: Whitelist = Whitelist.none
+  whiteList.addTags("div", "p", "h1", "h2", "h3", "h4", "h5", "span", "ul", "li", "table", "tr", "td", "a", "strong", "br", "code", "hr")
+  whiteList.addAttributes("a", "href")
 
   def extract(html: String, lang: String = "all"): Option[PageInfo] = {
     //This is replacing the non-breaking space with a regular space
@@ -47,6 +53,7 @@ object Gander {
         //some mutability beauty
         postExtractionCleanup(node, lang)
         info.copy(cleanedText = Some(node.text()),
+                  structuredText = Some(Jsoup.clean(node.toString, whiteList).toString()),
                   links = extractLinks(node))
       }.getOrElse(info)
     }
